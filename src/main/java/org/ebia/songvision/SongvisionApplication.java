c4e001c510eac1051eed4b9fa216a22d1b726f35
DCay <atriumandvintrigol@gmail.com>
2019-12-02 21:02:54 +0200

    Initial Commit - Project Initialization

---
+++ src/main/java/org/ebia/songvision/SongvisionApplication.java
@@ -0,0 +1,11 @@
+package org.ebia.songvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongvisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SongvisionApplication.class, args);
+    }
+}
---
+++ src/test/java/org/ebia/songvision/SongvisionApplicationTests.java
@@ -0,0 +1,11 @@
+package org.ebia.songvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongvisionApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
